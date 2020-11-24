f4ecd7c50cd0394165c2db26127569a5b35e8662
ReaperOnDrugs <ReaperOnDrugs@users.noreply.github.com>
2020-11-25 00:42:02 +0100

    Sidebar started

--- js/init.js
+++ js/init.js
@@ -1,6 +1,23 @@
-function checkUserAgent(){
+window.onload = function checkUserAgent(){
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-        alert("fuck off normie");
+        document.getElementById("blocker").classList.add("blockedHUH");
     }
-    else alert("u good homie");
+}
+const selInd = document.getElementById("selectIndicator");
+const tab = document.querySelectorAll(".navigOptionC");
+
+function moveind(targ){
+    selInd.style.top = targ+"px";
+}
+tab.forEach((e) => {
+    e.addEventListener("mousemove", (em) =>{
+        moveind(em.target.offsetTop);
+    });
+});
+document.getElementById("selConnC").addEventListener("mouseleave", () => {
+    selInd.style.top = document.querySelector("._selectedTAB").offsetTop+"px";
+});
+function setTab(str){
+    document.querySelector("._selectedTAB").classList.remove("_selectedTAB");
+    document.getElementById(str+"C").classList.add("_selectedTAB");
 }
