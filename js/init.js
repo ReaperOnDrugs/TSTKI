3ce281ec09cb556f028ab0879ba30bc78ca30a6f
ReaperOnDrugs <ReaperOnDrugs@users.noreply.github.com>
2020-11-24 22:37:53 +0100

    k

---
+++ js/init.js
@@ -0,0 +1,6 @@
+function checkUserAgent(){
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+        alert("fuck off normie");
+    }
+    else alert("u good homie");
+}
